8c782c9febbcd519e675ef15fa5e1d4e7e92f5c7
michael j Swallower <nickamato608@gmail.com>
2022-08-01 17:17:18 -0600

    push app stuff

---
+++ push_app.R
@@ -0,0 +1,8 @@
+library(rsconnect)
+
+rsconnect::deployApp("C:/Users/xbox/Desktop/Rstuff/WoWPvPTracker")
+
+#rsconnect::setAccountInfo(name='nick-amato',
+ #                         token='8EFA1D210E0AAE62B18AEFB433D761C5',
+  #                        secret='MlQDAVClHrh2LkB+MHX4QfXU/vcU/jws2GECW443')
+
